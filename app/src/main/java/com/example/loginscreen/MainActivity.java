033ca6414d43a98cb4c01d72bd47fd0327d5fb42
Tashi Jain <tashjain88@gmail.com>
2020-06-14 15:09:36 -0700

    initial commit

---
+++ app/src/main/java/com/example/loginscreen/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        Log.i("Values", usernameEditText.getText().toString());
+        Log.i("Values", passwordEditText.getText().toString());
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
